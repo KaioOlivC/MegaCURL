9cc0522fb2316742c8fde69aa6656bcd62a410e4
KaioOlivC <kaio.oliv.carvalho@gmail.com>
2024-06-19 12:06:17 -0300

    Scripts can edit DB now

--- eleito/scripts/populate-akina.sh
+++ eleito/scripts/populate-akina.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-password=$(cat .password.txt)
+dbPrompt=$(cat .dbPrompt.txt)
+dbName=$(cat .dbName.txt)
 
 cd ../routes
 
 ./user-register-akina.sh
+# Confirm email
+$dbPrompt 'UPDATE Users SET isEmailConfirmed=1;' $dbName
 ./user-login-akina.sh
 ./filliation-filliate-akina.sh
 ./candidacy-candidate-akina.sh
