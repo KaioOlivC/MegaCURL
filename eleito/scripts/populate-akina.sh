3d7a08f16ec742ebd90a1d36583a6c3157343eac
KaioOlivC <kaio.oliv.carvalho@gmail.com>
2024-06-19 12:19:16 -0300

    Now we can control db

--- eleito/scripts/populate-akina.sh
+++ eleito/scripts/populate-akina.sh
@@ -6,7 +6,7 @@ cd ../routes
 
 ./user-register-akina.sh
 # Confirm email
-$dbPrompt 'UPDATE Users SET isEmailConfirmed=1;' $dbName
+$dbPrompt 'UPDATE Users SET isEmailConfirmed=1' $dbName
 ./user-login-akina.sh
 ./filliation-filliate-akina.sh
 ./candidacy-candidate-akina.sh
